e8c11222276b86f7cc56e8416e9f969d8d1b9d3d
Mesak Mychart <mesakhpurba1@gmail.com>
2025-10-03 11:11:26 +0700

    Menambahkan proyek Praktikum 3 - LaraPress

---
+++ P3/LaraPress/resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    <p>Ini adalah halaman kontak untuk proyek LaraPress.</p>
+    <ul>
+        <li>Email: Pyramidhead7@gmail.com</li>
+        <li>Telepon: 081310387678</li>
+    </ul>
+    <a href="/">Kembali ke Halaman Utama</a>
+    <br>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+</body>
+</html>
